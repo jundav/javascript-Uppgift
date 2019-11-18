8868b375f54cf6b0fb1dcf9b80d5825490d97091
Dawit Kidane <daveking7@yahoo.com>
2019-11-18 22:32:55 +0100

    Lektion-1

---
+++ lektion-1/console/script.js
@@ -0,0 +1,30 @@
+// en kommentar kan skrivas såhär
+/* en kommentar kan skrivas såhär */
+
+/* 
+    märkspråk           skript-språk        programmeringsspråk
+    HTML                JavaScript          C
+    CSS                 PowerShell          C++
+    XML                 cmdLets             C#
+                        (python)            Java
+                                            (python)
+    Java är inte Javascript!
+    Högnivå språk (mellan användaren och systemet)
+    ------------------------------------------------
+    (Javascript)
+    Python
+    C# / Java
+    Delphi
+    Lågnivå språk (mellan systemet och hårdvara)
+    ------------------------------------------------
+    C++
+    C
+    Cobol
+    Ansembler
+    Mikrokod
+*/
+
+document.body.innerText = "Detta är en txt";
+console.log('här skriver vi ut nått i konsolen');
+console.warn('Detta är en varning!');
+console.error('Detta är ett felmeddelande');
